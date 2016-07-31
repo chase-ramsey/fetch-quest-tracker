68a70a07cbe8bf1ef6acdb4faa9f5bc8d1eecd39
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-31 15:14:53 -0500

    Built out add_quest, remove_quest, and write_char

--- lootbag.py
+++ lootbag.py
@@ -21,18 +21,32 @@ class Lootbag:
     game = input('> ')
     print('What level is {0}?'.format(name))
     level = input('> ')
-    print('How much xp does {0} currently have?'.format(name))
+    print('How much XP does {0} currently have?'.format(name))
     starting_xp = input('> ')
     return Character(name, game, level, starting_xp)
 
   def write_character_file(self):
-    pass
+    with open('character.txt', 'w+') as file:
+      pickle.dump(self.character, file)
 
-  def add_quest(self, person, item, xp):
-    pass
+  def add_quest(self):
+    print('Who are you questing for?')
+    person = input('> ')
+    print('What are you fetching for {0}'.format(person))
+    item = input('> ')
+    print('How much XP will you receive when you complete the quest?')
+    xp = input('> ')
+    new_quest = Quest(person, item, xp)
+    self.character.quests.append(new_quest)
 
-  def remove_quest(self, person):
-    pass
+  def remove_quest(self):
+    print('Who is the quest for?')
+    qp = input('> ')
+    print('What item were you fetching for {0}?'.format(qp))
+    qi = input('> ')
+    for quest in self.character.quests:
+      if quest.person == qp and quest.item == qi:
+        self.character.quests.remove(quest)
 
   def find_quest_item(self, item):
     pass
