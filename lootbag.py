62df22cea01d0f5a3361a395a7a31bdf57026fd8
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-31 15:57:54 -0500

    Finished building out lootbag

--- lootbag.py
+++ lootbag.py
@@ -29,27 +29,69 @@ class Lootbag:
     with open('character.txt', 'w+') as file:
       pickle.dump(self.character, file)
 
-  def add_quest(self):
-    print('Who are you questing for?')
-    person = input('> ')
-    print('What are you fetching for {0}'.format(person))
-    item = input('> ')
-    print('How much XP will you receive when you complete the quest?')
-    xp = input('> ')
-    new_quest = Quest(person, item, xp)
-    self.character.quests.append(new_quest)
+  def add_quest(self, *args):
+    if len(args) > 0 and len(args) < 3:
+      print('Looks like you don\'t have all the info needed to add a quest. Try again, or type "python lootbag.py" with no arguments if you\'ve forgotten what you need.')
+    else:
+      if len(args) == 0:
+        print('Who are you questing for?')
+        person = input('> ')
+        print('What are you fetching for {0}'.format(person))
+        item = input('> ')
+        print('How much XP will you receive when you complete the quest?')
+        xp = input('> ')
+      elif len(args) == 3:
+        person = args[0]
+        item = args[1]
+        xp = args[2]
+      new_quest = Quest(person, item, xp)
+      self.character.quests.append(new_quest)
 
-  def remove_quest(self):
-    print('Who is the quest for?')
-    qp = input('> ')
-    print('What item were you fetching for {0}?'.format(qp))
-    qi = input('> ')
+  def remove_quest(self, *args):
+    if len(args) > 0 and len(args) < 2:
+      print('Looks like you don\'t have all the info needed to add a quest. Try again, or type "python lootbag.py" with no arguments if you\'ve forgotten what you need.')
+    else:
+      if len(args) == 0:
+        print('Who is the quest for?')
+        qp = input('> ')
+        print('What item were you fetching for {0}?'.format(qp))
+        qi = input('> ')
+      elif len(args) == 2:
+        qp = args[0]
+        qi = args[1]
+      for quest in self.character.quests:
+        if quest.person == qp and quest.item == qi:
+          self.character.quests.remove(quest)
+
+  def find_quest_item(self, *args):
+    if len(args) == 0:
+      print('What item did you find?')
+      item = input('> ')
+      print('Who are you delivering the item to?')
+      person = input('> ')
+    elif len(args) == 2:
+      person = args[0]
+      item = args[1]
     for quest in self.character.quests:
-      if quest.person == qp and quest.item == qi:
-        self.character.quests.remove(quest)
+      if quest.person == person and quest.item == item:
+        quest.mark_item_found()
 
-  def find_quest_item(self, item):
-    pass
-
-  def fulfill_quest(self, person):
-    pass
+  def fulfill_quest(self, *args):
+    if len(args) == 0:
+      print('What item did you deliver?')
+      item = input('> ')
+      print('Who did you fetch the item for?')
+      person = input('> ')
+    elif len(args) == 2:
+      person = args[0]
+      item = args[1]
+    for quest in self.character.quests:
+      if quest.person == person and quest.item == item:
+        if quest.found == True:
+          quest.mark_quest_fulfilled()
+        else:
+          print('That item hasn\'t been marked as found yet. Would you like to mark it as found and fulfill the quest? [yes / no]')
+          answer = input('> ')
+          if answer.lower() == 'yes':
+            quest.mark_item_found()
+            quest.mark_quest_fulfilled()
